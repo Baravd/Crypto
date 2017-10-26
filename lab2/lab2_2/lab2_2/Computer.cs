8b37e196b3407f33b7a3676472cf866c96569368
bara <bara@mediamarktsaturn.com>
2017-10-26 22:02:36 +0300

    labs new remote

---
+++ lab2/lab2_2/lab2_2/Computer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+
+namespace lab2_2
+{
+    class Computer
+    {
+
+        public int gcdScaderi(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            }
+            return a;
+
+        }
+        public int gcdEuclid(int a, int b)
+        {
+            int temp = 0;
+            if (a < b)
+            {
+                temp = a;
+                a = b;
+                b = temp;
+            }
+            while (b != 0)
+            {
+                temp = a % b;
+                a = b;
+                b = temp;
+            }
+            return a;
+        }
+        public int gcdFor(int a, int b)
+        {
+            int d = 1;
+            int aux;
+            if (a > b)
+                aux = a;
+            else
+            {
+                aux = b;
+            }
+            for (int i = 1; i < Convert.ToInt32(Math.Sqrt(aux)); i++)
+            {
+                if (aux % i == 0)
+                {
+                    d = i;
+                }
+            }
+
+            return d;
+        }
+        public BigInteger gcdNrMari(BigInteger a , BigInteger b)
+        {
+            BigInteger temp = 0;
+            if (a < b)
+            {
+                temp = a;
+                a = b;
+                b = temp;
+            }
+            while (b != 0)
+            {
+                temp = a % b;
+                a = b;
+                b = temp;
+            }
+            return a;
+
+        }
+    }
+}
---
+++ lab2/lab2_2/lab2_2/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using System.Diagnostics;
+
+namespace lab2_2
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            int nr1 = 2135234;
+            int nr2 = 546;
+            byte[] nrMare1 = { 7, 7, 7, 6, 7, 6, 7, 7, 2, 7, 7, 7, 1, 0, 2, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 5, 3, 7, 6, 7};
+            byte[] nrMare2 = { 11, 7, 2, 7, 4, 7, 6, 7, 5};
+
+            Computer computer = new Computer();
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            int aux = computer.gcdEuclid(nr1, nr2);
+            sw.Stop();
+            long microseconds1 = sw.ElapsedTicks / (Stopwatch.Frequency / (1000L * 1000L));
+
+            sw.Start();
+            aux = computer.gcdScaderi(nr1, nr2);
+            sw.Stop();
+            long microseconds2 = sw.ElapsedTicks / (Stopwatch.Frequency / (1000L * 1000L));
+
+            sw.Start();
+            aux = computer.gcdFor(nr1, nr2);
+            sw.Stop();
+            long microseconds3 = sw.ElapsedTicks / (Stopwatch.Frequency / (1000L * 1000L));
+
+
+
+            BigInteger bigInteger = new BigInteger(nrMare1);
+            this.Text = bigInteger.ToString();
+            BigInteger bigInteger2 = new BigInteger(nrMare2);
+
+            sw.Start();
+            BigInteger aux2 = computer.gcdNrMari(bigInteger, bigInteger2);
+            sw.Stop();
+            long microseconds4 = sw.ElapsedTicks / (Stopwatch.Frequency / (1000L * 1000L));
+
+            chart1.Series["GCD"].Points.AddXY("Euclid", microseconds1);
+            chart1.Series["GCD"].Points.AddXY("Scaderi", microseconds2);
+            chart1.Series["GCD"].Points.AddXY("For SQRT", microseconds3);
+            chart1.Series["GCD"].Points.AddXY("Euclid nr mari", microseconds4);
+
+
+
+
+        }
+
+    }
+}
